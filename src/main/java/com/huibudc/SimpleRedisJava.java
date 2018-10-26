42a6e4bf557e44b34118df0b380f4480588b89dd
huibudc <xiang1990_ok@126.com>
2018-10-26 23:21:11 +0800

    init

---
+++ src/main/java/com/huibudc/SimpleRedisJava.java
@@ -0,0 +1,35 @@
+package com.huibudc;
+
+import redis.clients.jedis.Jedis;
+
+
+public class SimpleRedisJava {
+    private final static Jedis jedis = new Jedis("localhost");
+
+    public static void main(String[] args) {
+        System.out.println("is server running ? " + isServerRunning());
+        prepareData();
+        //get string data
+        System.out.println("the value of name is " + jedis.get("name"));
+        System.out.println("the value of job is " + jedis.get("job"));
+
+        // get list data
+        jedis.lrange("site-list", 0, 2).forEach(System.out::println);
+        //get keyset
+        jedis.keys("*").forEach(System.out::println);
+    }
+
+    private static boolean isServerRunning() {
+        return "PONG".equals(jedis.ping());
+    }
+
+    private static void prepareData() {
+        //string data
+        jedis.set("name", "leon");
+        jedis.set("job", "software engineer");
+        //list data
+        jedis.lpush("site-list", "Runoob");
+        jedis.lpush("site-list", "Google");
+        jedis.lpush("site-list", "Taobao");
+    }
+}
